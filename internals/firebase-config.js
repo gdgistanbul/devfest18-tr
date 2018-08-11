aaf475d1d0d3237b906d0521454da87a43c00e6b
Erol Kaftanoglu <erolkaftanoglu@gmail.com>
2018-08-11 15:24:27 +0300

    update

--- internals/firebase-config.js
+++ internals/firebase-config.js
@@ -1,5 +1,5 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../serviceAccount';
+import serviceAccount from '../serviceAccount.json';
 
 let firestore = null;
 export function initializeFirebase() {
